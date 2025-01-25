fbf283f73c7f50b8de859a31911497eaa0ca7ccf
Saobia3i <islamsaobia@gmail.com>
2025-01-25 13:00:35 +0600

    Removed unnecessary file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Navbar from './Components/Navbar/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 
 const App = () => {
