0a5aec993122182865e8bbd9438ac4e20f6ed8d9
Tahmid Khan <tahmid.khan.shuvo@gmail.com>
2025-01-26 02:50:27 +0600

    Upgraded Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import logo_light from '../../assets/logo-black.png';
 import logo_dark from '../../assets/logo-white.png';
 import toggle_light from '../../assets/night.png';
 import toggle_dark from '../../assets/day.png';
+import { FaHome, FaSearch, FaComment, FaUserCircle, FaPlusCircle } from 'react-icons/fa'; // Imported icon library
 
 const Navbar = ({ theme, setTheme }) => {
   const navigate = useNavigate(); // Move navigate here to avoid it being inside toggle_mode
@@ -14,20 +15,33 @@ const Navbar = ({ theme, setTheme }) => {
     setTheme(theme === 'light' ? 'dark' : 'light'); // Toggle theme between light and dark
   };
 
-  const handleButtonClick = () => {
-    navigate('/search'); // Navigate to the product search page
+  const handleButtonClick = (page) => {
+    navigate(page); // Navigate to the specific page passed in the argument
   };
 
   return (
     <div className='navbar'>
-      <img src={theme === 'light' ? logo_light : logo_dark} alt='logo' className='logo' />
+      <img src={theme === 'light' ? logo_light : logo_dark} alt='logo' className='logo'  />
       <ul>
-        <li className='ads'> <button onClick={handleButtonClick}>All Ads</button></li>
-        <li className='chat'>Chat</li>
-        <li className='account'>Account</li>
-        <li><button>Post Ads</button></li>
+        <li className='ads'>
+          <button onClick={() => handleButtonClick('/search')}><FaSearch /></button>
+        </li>
+        <li className='chat'>
+          <button onClick={() => handleButtonClick('/chat')}><FaComment /></button>
+        </li>
+        <li>
+          <button onClick={() => handleButtonClick('/account')}><FaUserCircle /></button>
+        </li>
+        <li>
+          <button onClick={() => handleButtonClick('/post')}><FaPlusCircle /></button>
+        </li>
       </ul>
-      <img onClick={toggle_mode} src={theme === 'light' ? toggle_light : toggle_dark} alt='toggle icon' className='toggle-icon' />
+      <img
+        onClick={toggle_mode}
+        src={theme === 'light' ? toggle_light : toggle_dark}
+        alt='toggle icon'
+        className='toggle-icon'
+      />
     </div>
   );
 };
