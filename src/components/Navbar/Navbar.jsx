969f6b3890377bb454f48dee28f918c37ebf8eba
Tahmid Amir <techlearn715@gmail.com>
2025-01-27 01:18:31 +0600

    Navbar icon space with chat

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,34 +6,46 @@ import logo_light from '../../assets/logo-black.png';
 import logo_dark from '../../assets/logo-white.png';
 import toggle_light from '../../assets/night.png';
 import toggle_dark from '../../assets/day.png';
-import { FaHome, FaSearch, FaComment, FaUserCircle, FaPlusCircle } from 'react-icons/fa'; // Imported icon library
+import { FaHome, FaSearch, FaComment, FaUserCircle, FaPlusCircle } from 'react-icons/fa';
 
 const Navbar = ({ theme, setTheme }) => {
-  const navigate = useNavigate(); // Move navigate here to avoid it being inside toggle_mode
+  const navigate = useNavigate();
 
   const toggle_mode = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light'); // Toggle theme between light and dark
+    setTheme(theme === 'light' ? 'dark' : 'light');
   };
 
   const handleButtonClick = (page) => {
-    navigate(page); // Navigate to the specific page passed in the argument
+    navigate(page);
   };
 
   return (
     <div className='navbar'>
-      <img src={theme === 'light' ? logo_light : logo_dark} alt='logo' className='logo'  />
+      <img src={theme === 'light' ? logo_light : logo_dark} alt='logo' className='logo' />
       <ul>
         <li className='ads'>
-          <button onClick={() => handleButtonClick('/search')}><FaSearch /></button>
+          <button onClick={() => handleButtonClick('/search')}>
+            <FaSearch className="icon" />
+            <span className="icon-text">Search</span>
+          </button>
         </li>
         <li className='chat'>
-          <button onClick={() => handleButtonClick('/chat')}><FaComment /></button>
+          <button onClick={() => handleButtonClick('/chat')}>
+            <FaComment className="icon" />
+            <span className="icon-text">Chat</span>
+          </button>
         </li>
         <li>
-          <button onClick={() => handleButtonClick('/account')}><FaUserCircle /></button>
+          <button onClick={() => handleButtonClick('/account')}>
+            <FaUserCircle className="icon" />
+            <span className="icon-text">Account</span>
+          </button>
         </li>
         <li>
-          <button onClick={() => handleButtonClick('/postAd')}><FaPlusCircle /></button>
+          <button onClick={() => handleButtonClick('/postAd')}>
+            <FaPlusCircle className="icon" />
+            <span className="icon-text">Post Free Ads</span>
+          </button>
         </li>
       </ul>
       <img
