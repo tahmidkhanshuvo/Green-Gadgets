3b474f65f4cc7497e4ce1b0e408f83ad4e3a7fd4
Saobia3i <islamsaobia@gmail.com>
2025-02-22 02:03:43 -0800

    adding blogs= page,details,form

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Vite configuration for React
+export default defineConfig({
+  plugins: [react()],
+});
